3e2b9c64caa684036a02ba97470d4561901d5323
suki-9 <suki.music2004@gmail.com>
2023-12-05 15:01:18 +0900

    create: "/api/endpointsにresponseを追加"

--- backend/src/api_endpoints/endpoints.rs
+++ backend/src/api_endpoints/endpoints.rs
@@ -1,18 +1,23 @@
 use axum::{
   routing::post,
   Router,
+  response::{
+    IntoResponse,
+    Json,
+  }
 };
+
+use serde_json::json;
 
 pub fn endpoint_routes() -> Router {
   Router::new()
     .route("/endpoint", post(endpoint))
-    .route("/endpoints", post(endpoints))
+    .route("/endpoints", post(endpoint))
 }
 
-async fn endpoint() -> &'static str {
-  "endpoint"
-}
-
-async fn endpoints() -> &'static str {
-  "endpoints"
+async fn endpoint() -> impl IntoResponse {
+  Json(json!({
+    "error": "Unsupported endpoint",
+    "summary": "Kurageではこのエンドポイントの実装予定はありません。\n理由: 使いどころが無い為"
+  }))
 }
