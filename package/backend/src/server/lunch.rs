7ff39c6d9c5637954c37557ff2fe5ff04e6d03b0
suki-9 <suki.music2004@gmail.com>
2024-10-25 06:20:59 +0900

    refactor(backend): allow_headerへの引数をAnyに

--- package/backend/src/server/lunch.rs
+++ package/backend/src/server/lunch.rs
@@ -1,6 +1,5 @@
 use super::api;
 use axum::{http::HeaderValue, routing::get, Router};
-use hyper::header::CONTENT_TYPE;
 use tower_http::cors::{Any, CorsLayer};
 
 pub async fn lunch_server(port: i32) -> () {
@@ -12,7 +11,7 @@ pub async fn lunch_server(port: i32) -> () {
       CorsLayer::new()
         .allow_origin("*".parse::<HeaderValue>().unwrap())
         .allow_methods(Any)
-        .allow_headers(vec![CONTENT_TYPE]),
+        .allow_headers(Any),
     ),
   )
   .await
